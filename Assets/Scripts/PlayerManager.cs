33c9187e52a502e2409f1238dcc72bad9c3ed77b
nickdaniel5247 <146045599+nickdaniel5247@users.noreply.github.com>
2025-03-16 14:50:07 -0700

    Add detector triggers

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,9 +3,41 @@ using UnityEngine;
 
 public class PlayerManager : NetworkBehaviour
 {
+    private GameManager gameManager;
+
     [Rpc(SendTo.Everyone)]
     public void SetPlayerStatusRPC(bool enabled)
     {
         gameObject.SetActive(enabled);
+    }
+
+    private void Awake()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner)
+        {
+            return;
+        }
+
+        //All are disabled by defualt except our own to save unnecessary computation
+        GetComponentInChildren<BoxCollider>().enabled = true;
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        //Only continue if we are chosen
+        if (gameManager.currentChosen.Value != NetworkManager.Singleton.LocalClientId)
+        {
+            return;
+        }
+
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("SEND RPC");
+        }
     }
 }
