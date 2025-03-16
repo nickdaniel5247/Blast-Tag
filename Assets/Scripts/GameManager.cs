dc5d416adfffe9943bdbf6d957c7c54d2eea9914
nickdaniel5247 <146045599+nickdaniel5247@users.noreply.github.com>
2025-03-16 16:57:59 -0700

    Add timed round eliminations

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,14 @@ public class GameManager : NetworkBehaviour
     public NetworkVariable<ulong> currentChosen = new NetworkVariable<ulong>();
     public NetworkVariable<bool> playing = new NetworkVariable<bool>(false);
 
+    public float roundTime = 10f;
+    private float currentRoundTime = 0f;
+
+    //Synced as int to allow for less data transfers
+    private NetworkVariable<int> currentRoundTimeSync = new NetworkVariable<int>();
+
+    private int alivePlayers = 0;
+
     public void StartGame()
     {
         if (!IsServer)
@@ -15,18 +23,24 @@ public class GameManager : NetworkBehaviour
             return;
         }
 
+        //Count all the players
+        alivePlayers = 0;
+
         //Make sure all players are enabled in case of previous rounds
         foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
         {
             var playerObject = NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).gameObject;
             playerObject.GetComponent<PlayerManager>().SetPlayerStatusRPC(true);
+            ++alivePlayers;
         }
 
         int randomIdx = Random.Range(0, NetworkManager.Singleton.ConnectedClientsIds.Count);
         ulong chosenClientID = NetworkManager.Singleton.ConnectedClientsIds[randomIdx];
 
+        //Reset these variables
         currentChosen.Value = chosenClientID;
         playing.Value = true;
+        currentRoundTime = 0f;
     }
 
     public void Tag(Collider collision, GameObject tagger)
@@ -48,12 +62,36 @@ public class GameManager : NetworkBehaviour
         currentChosen.Value = taggedID;
     }
 
+    private void EndRound()
+    {
+        var playerObject = NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(currentChosen.Value).gameObject;
+        playerObject.GetComponent<PlayerManager>().SetPlayerStatusRPC(false);
+
+        --alivePlayers;
+    }
+
     public void Update()
     {
-        if (!playing.Value)
+        if (!IsServer || !playing.Value)
         {
             return;
         }
+
+        if (currentRoundTime > roundTime)
+        {
+            currentRoundTime = 0;
+            EndRound();
+        }
+
+        if (alivePlayers == 1)
+        {
+            playing.Value = false;
+            StartCoroutine(WaitToStartGame());
+            return;
+        }
+
+        currentRoundTime += Time.deltaTime;
+        currentRoundTimeSync.Value = (int)currentRoundTime;
     }
 
     IEnumerator WaitToStartGame()
